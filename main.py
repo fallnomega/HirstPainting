94691c9ba390f8be9c5ba76d03ea2c9434a0e775
fallnomega <fallnomega@gmail.com>
2023-02-03 12:52:47 -0800

    init commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# ###This code will not work in repl.it as there is no access to the colorgram package here.###
+# ##We talk about this in the video tutorials##
+# import colorgram
+# import turtle as t
+#
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 30)
+# for color in colors:
+#     temp = (color.rgb.r, color.rgb.b, color.rgb.g)
+#     rgb_colors.append(temp)
+
+
+color_pallet = [(202, 110, 164), (149, 50, 75), (222, 136, 201), (53, 123, 93), (170, 41, 154), (138, 20, 31),
+                (134, 184, 163), (197, 73, 92), (47, 86, 121), (73, 35, 43), (145, 149, 178), (14, 70, 98),
+                (232, 165, 176), (160, 158, 142), (54, 50, 45), (101, 77, 75), (183, 171, 205), (36, 74, 60),
+                (19, 89, 86),  (82, 129, 148), (147, 19, 17), (27, 102, 68), (12, 64, 70), (107, 153, 127),
+                (176, 208, 192), (168, 102, 99)]
+
+# print (rgb_co lors)
